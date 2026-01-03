918114863be6f189f3508dbb394499543a64c927
Fankekeke <1258846421@qq.com>
2026-01-03 23:07:11 +0800

    feat - 系统初始化

---
+++ backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package cc.mrbird.febs.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project/20251116基于大语言模型的老物件回收与修复系统/db/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
